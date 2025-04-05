a1b5c68ec1fa15259df13ba337d931e574d3c0bf
PeihengLu <lupeihenged@gmail.com>
2025-04-05 09:30:46 +0800

    [db] reorganize the sql scripts

--- compress.sh
+++
@@ -1,6 +0,0 @@
-# compress all files larger than 50mb
-find . -type f -size +50M -exec zip {}.zip {} \; 
-# recreate gitignore
-rm .gitignore
-# add all files larger than 50mb to .gitignore without the ./ prefix but with the directory prefix
-find . -type f -size +50M -exec echo {} \; | sed 's/^\.\///' >> .gitignore
--- setup.sh
+++
@@ -1,2 +0,0 @@
-# deflate all zip files in their original directory
-find . -type f -name "*.zip" -exec unzip -o {} \;
